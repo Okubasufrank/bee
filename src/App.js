c0e6462e1dcdef8825cd7f13e26cc1fef61e628b
Okubasufrank <franklineokubasu51@gmail.com>
2022-10-18 01:20:03 +0300

    created removeCharacter method to use with state for removing/ editing content in the array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import Table from './Table';
 
 class App extends React.Component {
-  render(){
-    return (
-      <div className='App'>
-        <h1>Hello React</h1>
-      </div>
-    )
-  }
+    state = {
+        characters: [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ]
+    }
+
+    removeCharacter = (index => {
+        const { characters } = this.state
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            })
+        })
+    })
+
 }
 
 export default App;
